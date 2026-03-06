80c83ccceb1b44ce3d4c034dd5f26413332cea33
OtavioGelain <otavioluizgelain@gmail.com>
2026-03-06 09:28:20 -0300

    adding cors verification

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,19 @@ import cors from 'cors';
 import router from './routes/index-routes';
 
 export const app = express()
+const whitelist = [
+    'http://localhost:5173',
+]
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin || whitelist.includes(origin)){
+            callback(null, true)
+        }else{
+            callback(new Error('Blocked by Cors'))
+        }
+    }
+
+}))
 app.use(router)
